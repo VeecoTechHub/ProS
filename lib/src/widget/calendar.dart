56bb97ba586931d83557b5ba2efac744e98db987
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-05-17 12:41:48 +0800

    calendar bug fix

--- lib/src/widget/calendar.dart
+++ lib/src/widget/calendar.dart
@@ -214,7 +214,7 @@ class _ProZCalendarState extends State<ProZCalendar> {
         date: date,
         label: date.day.toString(),
         type: CalendarCellType.current,
-        enable: firstDay == null ? true : date.isAfter(firstDay ?? DateTime.now()),
+        enable: firstDay == null ? true : date.isAfter(firstDay?.subtract(const Duration(days:1)) ?? DateTime.now()),
       ));
     }
 
