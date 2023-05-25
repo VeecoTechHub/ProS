420e366934e395264c7da6059914c6ff827218d9
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-05-26 00:37:14 +0800

    new init

--- lib/src/init.dart
+++ lib/src/init.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
+import 'package:pro_z/src/services/services.dart';
 import 'package:pro_z/src/store/store_index.dart';
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 class ProZ {
   static Future init() async {
     Get.put<HttpSetting>(HttpSetting());
+    StreamingSharedPreferences sharedPreferences =
+        await StreamingSharedPreferences.instance;
+    Get.put<StorageService>(StorageService(sharedPreferences));
   }
 }
